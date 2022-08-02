b25ecb772d91edc0a2d13d6e0fd4d77cafad5377
DrRuski <ilia_e@hotmail.com>
2022-08-02 12:32:40 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 function openNav() {
-    document.getElementById("nav-container").style.width="15rem";
+    document.getElementById("navContainer").style.width="15rem";
 }
 
 function closeNav() {
-    document.getElementById("nav-container").style.width="0";
+    document.getElementById("navContainer").style.width="0";
+}
+
+function navFunction() {
+    document.getElementById("navDropdown").classList.toggle("show-dropdown")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".dropdown-btn")) {
+        var dropdowns = document.getElementsByClassName("features-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show-dropdown")) {
+                openDropdown.classList.remove("show-dropdown");
+            }
+        }
+    }
 }
