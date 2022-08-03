c0b52ec5e88050c3c86238fe56bc76875ce548e3
DrRuski <ilia_e@hotmail.com>
2022-08-03 19:23:00 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+/* Sidenav */
+
 function openNav() {
     document.getElementById("navContainer").style.width="15rem";
 }
@@ -6,19 +9,22 @@ function closeNav() {
     document.getElementById("navContainer").style.width="0";
 }
 
-function navFunction() {
-    document.getElementById("navDropdown").classList.toggle("show-dropdown")
-}
 
-window.onclick = function(event) {
-    if (!event.target.matches(".dropdown-btn")) {
-        var dropdowns = document.getElementsByClassName("features-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show-dropdown")) {
-                openDropdown.classList.remove("show-dropdown");
-            }
+
+
+/* Dropdown */
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display="block";
         }
-    }
+    })
 }
